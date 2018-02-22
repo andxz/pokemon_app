a927ac8a4f852ba97ff760e7c271f202e59e96b9
andxz <andrez.romero@hotmail.com>
2018-02-22 14:41:10 +0100

    almost done

--- main.js
+++ main.js
@@ -1,81 +1,125 @@
+    /*close button*/
+
+var myNodelistDelete = document.getElementsByTagName("li");
+var i;
+for (i = 0; i < myNodelistDelete.length; i++) {
+  var deleteButton = document.createElement("span");
+  var txt = document.createTextNode("RADERA");
+  deleteButton.className = "close";
+  deleteButton.appendChild(txt);
+  myNodelistDelete[i].appendChild(deleteButton);
+
+
+}
+
+
+/*move button*/
+var myNodelistDone = document.getElementsByTagName("li");
+var i;
+for (i = 0; i < myNodelistDone.length; i++) {
+  var doneButton = document.createElement("span");
+  var txt = document.createTextNode("KLAR");
+  doneButton.className = "done";
+  doneButton.appendChild(txt);
+  myNodelistDone[i].appendChild(doneButton);
+}
+
 /* First function receives information from input and transforms into a new li everytime
 we click submit */
 
  
 
 function input_Todo() {
+    
+
+
+
 
     var text = document.getElementById("submit_todo").value;
-    var node = document.createElement("li");
     var textnode = document.createTextNode(text);
+    var node = document.createElement("li");
+
     node.appendChild(textnode);
     document.getElementById("todo").appendChild(node);
 
-    
-    /*for each loop that moves the item from Todo to Done Todos-list */
+/* lägger till delete button på node från inputfält */
 
-
-    const todo = document.getElementById('todo');
-    const done = document.getElementById('done_todo')
-    const listItems = document.querySelectorAll('li');
-
-    for (const item of listItems) {
-        item.addEventListener('click', function () {
-            todo.removeChild(this);
-            done.appendChild(this);
-
-        })
-    }
-
-
-
-}
-
-    /*for each loop that moves the item from Todo to Done Todos-list */
-
-
-    const todo = document.getElementById('todo');
-    const done = document.getElementById('done_todo')
-    const listItems = document.querySelectorAll('li');
-
-    for (const item of listItems) {
-        item.addEventListener('click', function () {
-            todo.removeChild(this);
-            done.appendChild(this);
-
-        })
-    }
-
- 
-
-
-
-
-
-/*close button*/
-
-var myNodelist = document.getElementsByTagName("li");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("span");
+  var deleteButton = document.createElement("span");
   var txt = document.createTextNode("RADERA");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
+  deleteButton.className = "close";
+  deleteButton.appendChild(txt);
+
+    node.appendChild(deleteButton);
+
+    
+/* lägger till done button på node från inputfält */
+
+  var doneButton = document.createElement("span");
+  var txt = document.createTextNode("KLAR");
+  doneButton.className = "done";
+  doneButton.appendChild(txt);
+    
+    node.appendChild(doneButton);
+
+    
 
 
-/*move button*/
+    /*for each loop that moves the item from Todo to Done Todos-list */
 
-var myNodelist = document.getElementsByTagName("li");
+
+    const todo = document.getElementById('todo');
+    const done = document.getElementById('done_todo')
+    const listItems = document.querySelectorAll('li');
+
+    for (const item of listItems) {
+        item.addEventListener('click', function () {
+            todo.removeChild(this);
+            done.appendChild(this);
+
+        })
+    }
+
+
+/* Function that deletes Todo that you click on */
+
+    var close = document.getElementsByClassName("close");
 var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("span");
-  var txt = document.createTextNode("KLAR  ");
-  span.className = "done";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
 }
+
+
+
+
+
+}
+
+
+
+
+
+
+    /*for each loop that moves the item from Todo to Done Todos-list */
+
+
+    const todo = document.getElementById('todo');
+    const done = document.getElementById('done_todo');
+    const listItems = document.querySelectorAll('li');
+
+    for (const item of listItems) {
+        item.addEventListener('click', function () {
+            todo.removeChild(this);
+            done.appendChild(this);
+
+        })
+
+
+
+
+    }
 
 
 /* Function that deletes Todo that you click on */
@@ -108,47 +152,3 @@ function delete_allDoneTodos() {
         list.removeChild(list.firstChild);
     }
 }
-
-
-
-
-
-
-
-/*
-
-
-
-
-
-
-function myFunction() {
-    var x = document.getElementById("submit_todo").value;
-    document.getElementById("todo").innerHTML = x;
-}
-
-
-/* varje gång man klickar blir det done
-
-
-const todo = document.getElementById('todo');
-const done = document.getElementById('done')
-const listItems = document.querySelectorAll('li');
-
-for(const item of listItems){
-    item.addEventListener('click', function(){
-        todo.removeChild(this);
-        done.appendChild(this);
-    })
-}
-
-/* varje gång man klickar li tas ett element bort
-const listItems = document.querySelectorAll('li');
-
-for(const item of listItems){
-    item.addEventListener('click', function(){
-        this.parentElement.removeChild(this);
-    })
-}
-
-t*/
